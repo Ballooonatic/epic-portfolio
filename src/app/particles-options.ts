f21f8765f7f2b17290c3f26e074ac5bc19c48817
Michael Imgrund <michael.imgrund@gmail.com>
2022-05-31 14:24:53 -0500

    i finally have the particle options the way i want them

--- src/app/particles-options.ts
+++ src/app/particles-options.ts
@@ -1,12 +1,7 @@
-import { MoveDirection, ClickMode, HoverMode, OutMode } from 'tsparticles-engine';
+import { MoveDirection, ClickMode, HoverMode, OutMode, CollisionMode, RotateDirection } from 'tsparticles-engine';
 
 export const particlesOptions = {
-    background: {
-        color: {
-            value: "#000"
-        }
-    },
-    fpsLimit: 120,
+    fpsLimit: 60,
     interactivity: {
         events: {
             onClick: {
@@ -14,60 +9,131 @@ export const particlesOptions = {
                 mode: ClickMode.repulse
             },
             onHover: {
+                parallax: {
+                    enable: true,
+                    force: 10,
+                    smooth: 20,
+                },
                 enable: true,
-                mode: HoverMode.slow
+                mode: HoverMode.trail
             },
-            resize: true
+            resize: true,
         },
         modes: {
+            trail: {
+                delay: 0.001,
+                quantity: 2,
+                pauseOnStop: true,
+            },
             repulse: {
                 distance: 200,
-                duration: 0.3,
-                speed: 4,
-            },
-            slow: { factor: 5 }
+                duration: 1,
+                speed: 0.1,
+            }
         }
     },
     particles: {
         color: {
-            value: "#ffffff"
+            animation: {
+                h: {
+                    // count: 1,
+                    enable: true,
+                    offset: 1,
+                    speed: 250,
+                    sync: false
+                },
+                s: {
+                    // count: 1,
+                    enable: true,
+                    offset: 1,
+                    speed: 250,
+                    sync: false
+                },
+                l: {
+                    // count: 1,
+                    enable: true,
+                    offset: 1,
+                    speed: 250,
+                    sync: false
+                },
+            },
+            value: '#fff',
         },
         links: {
             color: "#ffffff",
-            distance: 150,
+            distance: 100,
             enable: true,
-            opacity: 0.5,
-            width: 1
+            opacity: 0.1,
+            width: 1,
+            blink: true,
+            frequency: 0.5,
+            triangles: {
+                enable: true,
+                frequency: 0.1
+            },
+            consent: true,
+            warp: true,
         },
         collisions: {
-            enable: true
+            enable: true,
+            mode: CollisionMode.destroy
         },
         move: {
-            direction: MoveDirection.none,
+            direction: MoveDirection.bottomRight,
             enable: true,
-            outModes: {
-                default: OutMode.bounce
-            },
-            random: false,
-            speed: 6,
-            straight: false
+            random: true,
+            speed: 5,
+            collisions: true,
+            bounce: true,
+            outMode: OutMode.out,
+            decay: 0,
+            drift: 0,
+            warp: true,
         },
         number: {
-            density: {
-                enable: true,
-                area: 800
-            },
-            value: 80
+            value: 150,
+            max: 200, // ???
+            limit: 300
         },
-        opacity: {
-            value: 0.5
-        },
+        opacity: { value: 1 },
         shape: {
-            type: "circle"
+            stroke: { width: 1 },
+            type: [ 
+                "circle",
+                "edge",
+                "triangle",
+                "star",
+                "character",
+                "polygon"
+            ],
+            options: {
+                star: {
+                    sides: 4,
+                    inset: 2,
+                },
+                character: {
+                    value: "+", 
+                    font: "Verdana",
+                    weight: "bolder"
+                },
+                polygon: {
+                    sides: 6
+                }
+            }
         },
         size: {
-            value: {min: 1, max: 5 },
-        }
+            value: { min: 1, max: 3 },
+        },
+        rotate:		{
+            value: 100,
+            animation: {
+                enable: true,
+                speed: 100,
+                sync: false
+            },
+            direction:	RotateDirection.random,
+            path: false
+        },
     },
     detectRetina: true
 };
