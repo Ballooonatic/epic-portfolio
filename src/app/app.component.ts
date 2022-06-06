b1fb74665c5efe780dd296e7527b9d1163b9ebcd
Michael Imgrund <michael.imgrund@gmail.com>
2022-06-06 12:01:49 -0500

    having more animation fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import anime from 'animejs/lib/anime.es.js';
-import { Container, Engine } from "tsparticles-engine";
+import { colorMix, Container, Engine } from "tsparticles-engine";
 import { loadFull } from "tsparticles";
 import { particlesOptions } from './particles-options';
 
@@ -14,34 +14,84 @@ export class AppComponent implements AfterViewInit {
 
   id = "tsparticles";
   particlesOptions = particlesOptions;
+  michaelImgrund = 'MICHAEL IMGRUND'.split('')
+  digitalExperiences = 'digital experiences'.split('')
+  titleColor = -24;
 
   ngAfterViewInit() {
 
     anime({
+      targets: '.fancy-title',
+      translateY: [1, 0],
+      delay: anime.stagger(150),
+      rotate: function () { return anime.random( -3, 3 )},
+      easing: 'spring(10, 100, 0, 30)',
+    })
+
+    anime({
+      targets: '.fancy-title',
+      duration: 5000,
+      delay: 1500,
+      color: () => {
+        return `hsl(${this.titleColor += 24},${anime.random(60, 61)}%,${anime.random(50, 51)}%)`;
+      },
+      // color: function () {
+      //   return `hsl(${anime.random(0, 360)},${anime.random(20, 80)}%,${anime.random(30, 90)}%)`;
+      // },
+    })
+
+    anime({
       targets: '.title',
       translateX: window.innerWidth, // cool i can do this for responsiveness
+      easing: 'easeInElastic(1, .3)',
+      duration: function () { return anime.random( 1000, 3000 )},
+      endDelay: function () { return anime.random( 0, 500 )}, // need end delay bc reverse lol
+      direction: 'reverse'
+    })
+    
+    anime({
+      targets: '.subtitle',
+      translateX: -window.innerWidth,
       easing: 'easeInElastic',
       duration: function () { return anime.random( 1000, 3000 )},
       endDelay: function () { return anime.random( 0, 500 )}, // need end delay bc reverse lol
       direction: 'reverse'
     });
     
-    // anime({
-    //   targets: '.subtitle',
-    //   translateX: -window.innerWidth,
-    //   easing: 'easeInElastic',
-    //   duration: function () { return anime.random( 1000, 3000 )},
-    //   endDelay: function () { return anime.random( 0, 500 )}, // need end delay bc reverse lol
-    //   direction: 'reverse'
-    // });
-    
     anime({
       targets: '.fancy-subtitle-letter',
-      translateY: 50,
+      color: function () {
+        return `rgb(
+          ${anime.random(100, 256)},
+          ${anime.random(100, 256)},
+          ${anime.random(100, 256)}
+        )`;
+      },
+      translateY: 200,
+      rotate: anime.stagger([-360, 360]),
+      skew: anime.stagger([-60, 60]),
+      translateX: anime.stagger([-150, -100]),
+      scale: function () { return anime.random(1, 2) },
+      easing: 'easeInOutElastic(2, .4)',
+      // easing: 'spring(1, 100, 5, 0)',
+      duration: 3000,
       direction: 'alternate',
-      delay: anime.stagger(100),
-      easing: 'easeInSine'
+      loop: true,
+      delay: anime.stagger(100, {
+        start: 1000,
+        grid: [5, 10], // no idea how this works now lmao
+      }),
     });
+
+    anime({
+      targets: '.heart',
+      rotateY: 360,
+      rotateZ: 360,
+      duration: 3000,
+      easing: 'linear',
+      loop: true
+    })
+
   }
 
   // particlesLoaded(container: Container): void {
